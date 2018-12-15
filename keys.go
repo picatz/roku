d593e94026d39bac670613da5970b8e6031e9547
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-12-14 22:42:19 -0500

    Create keys.go

---
+++ keys.go
@@ -0,0 +1,58 @@
+package roku
+
+var (
+	// HomeKey reprents the "Home" button.
+	HomeKey = "Home"
+	// RevKey reprents the "Rev" button.
+	RevKey = "Rev"
+	// FwdKey reprents the "Fwd" button.
+	FwdKey = "Fwd"
+	// PlayKey reprents the "Play" button.
+	PlayKey = "Play"
+	// SelectKey reprents the "Select" button.
+	SelectKey = "Select"
+	// LeftKey reprents the "Left" button.
+	LeftKey = "Left"
+	// RightKey reprents the "Right" button.
+	RightKey = "Right"
+	// DownKey reprents the "Down" button.
+	DownKey = "Down"
+	// UpKey reprents the "Up" button.
+	UpKey = "Up"
+	// BackKey reprents the "Back" button.
+	BackKey = "Back"
+	// InstantReplayKey reprents the "InstantReplay" button.
+	InstantReplayKey = "InstantReplay"
+	// InfoKey reprents the "Info" button.
+	InfoKey = "Info"
+	// BackspaceKey reprents the "Backspace" button.
+	BackspaceKey = "Backspace"
+	// SearchKey reprents the "Search" button.
+	SearchKey = "Search"
+	// EnterKey reprents the "Enter" button.
+	EnterKey = "Enter"
+	// VolumeDownKey reprents the "VolumeDown" button.
+	VolumeDownKey = "VolumeDown"
+	// VolumeMuteKey reprents the "VolumeMute" button.
+	VolumeMuteKey = "VolumeMute"
+	// VolumeUpKey reprents the "VolumeUp" button.
+	VolumeUpKey = "VolumeUp"
+	// PowerOffKey reprents the "PowerOff" button.
+	PowerOffKey = "PowerOff"
+	// ChannelUpKey reprents the "ChannelUp" button.
+	ChannelUpKey = "ChannelUp"
+	// ChannelDownKey reprents the "ChannelDown" button.
+	ChannelDownKey = "ChannelDown"
+	// InputTunerKey reprents the "InputTuner" button.
+	InputTunerKey = "InputTuner"
+	// InputHDMI1Key reprents the "InputHDMI1" button.
+	InputHDMI1Key = "InputHDMI1"
+	// InputHDMI2Key reprents the "InputHDMI2" button.
+	InputHDMI2Key = "InputHDMI2"
+	// InputHDMI3Key reprents the "InputHDMI3" button.
+	InputHDMI3Key = "InputHDMI3"
+	// InputHDMI4Key reprents the "InputHDMI4" button.
+	InputHDMI4Key = "InputHDMI4"
+	// InputAV1Key reprents the "InputAV1" button.
+	InputAV1Key = "InputAV1"
+)
