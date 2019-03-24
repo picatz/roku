675826105fbc1ad6cdf8b6170ceb92305622ae2b
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-03-23 23:03:33 -0400

    Update apps.go
    add some builtin app ID numbers

--- apps.go
+++ apps.go
@@ -6,3 +6,23 @@ type apps struct {
 
 // Apps is a collection of one or more App(s)
 type Apps = []*App
+
+// Found using https://channelstore.roku.com/browse
+var (
+	// YouTubeAppID is the channel app ID number for Google's YouTube
+	YouTubeAppID = 837
+	// NetflixAppID is the channel app ID number for Netflix
+	NetflixAppID = 12
+	// PrimeVideoAppID is the channel app ID number for Amazon Prime Video
+	PrimeVideoAppID = 13
+	// FireFoxAppID is the channel app ID number for FireFox
+	FireFoxAppID = 47545
+	// HuluAppID is the channel app ID number for Hulu
+	HuluAppID = 2285
+	// PandoraAppID is the channel app ID number for Pandora
+	PandoraAppID = 28
+	// SpotifyAppID is the channel app ID number for Spotify
+	SpotifyAppID = 19977
+	// ESPNAppID is the channel app ID number for ESPN
+	ESPNAppID = 34376
+)
