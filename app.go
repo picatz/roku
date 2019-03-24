2f2551dccf33eedc0400f4245b8fcaeeb440e0df
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-03-23 23:09:48 -0400

    Update app.go

--- app.go
+++ app.go
@@ -4,7 +4,7 @@ package roku
 // https://github.com/kinghrothgar/roku/blob/master/roku/roku.go#L63
 type App struct {
 	Name    string `xml:",chardata" json:"name,omitempty"`
-	ID      string `xml:"id,attr" json:"id,omitempty"`
+	ID      int    `xml:"id,attr" json:"id,omitempty"`
 	Type    string `xml:"type,attr" json:"type,omitempty"`
 	SubType string `xml:"subtype,attr" json:"sub_type,omitempty"`
 	Version string `xml:"version,attr" json:"version,omitempty"`
